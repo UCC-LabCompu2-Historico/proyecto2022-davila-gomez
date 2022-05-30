2c9cd1b39005002dde78f36e5e7485aaaaf48d06
frun903 <frun903@gmail.com>
2022-05-30 16:48:00 -0300

    Agregamos JavaScript, Tipografia en css y cambios en html

---
+++ funcionalidad.js
@@ -0,0 +1,210 @@
+//Para solo 3 bandas
+
+/*Voy a declarar variables globales*/
+var banda_1=null;
+var banda_2=null;
+var multiplicadora=null;
+var Resultado;
+
+
+
+function ingreso_Banda_1(id, value)
+{
+    banda_1= document.getElementById('Color_Primera_Banda').value;
+
+    return banda_1;
+}
+
+
+
+function ingreso_Banda_2(id, value)
+{
+    banda_2= document.getElementById('Color_segunda_banda').value;
+
+    return banda_2;
+}
+
+function ingreso_Banda_multiplicadora(id, value)
+{
+    multiplicadora= document.getElementById('Banda_Multiplicador').value;
+
+    return multiplicadora;
+}
+
+
+
+/**
+     * Descripción de que hace la función
+     * @method calcular
+     * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+     * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+     * @return Resultado
+     */
+
+
+function calcular()
+{
+
+
+
+    //Empezamos con los switch
+
+    switch(banda_1)
+    {
+        case "Negro":
+            Resultado="0";
+            break;
+
+        case "Marron":
+            Resultado="1";
+            break;
+
+        case "Rojo":
+            Resultado="2";
+            break;
+        
+        case "Naranja":
+            Resultado="3";
+            break;
+
+        case "Amarillo":
+            Resultado="4";
+            break;
+
+        case "Verde":
+            Resultado="5";
+            break;
+
+        case "Azul":
+            Resultado="6";
+            break;
+
+        case "Violeta":
+            Resultado="7";
+            break;
+
+        case "Gris":
+            Resultado="8";
+            break;
+
+        case "Blanco":
+            Resultado="9";
+            break;
+        
+        default:
+            //Aqui que le mandara al usuario un aviso de que falta banda 1
+        break;
+
+    }
+
+    //Banda 2
+ 
+
+    switch(banda_2)
+    {
+        case "Negro":
+            Resultado=Resultado+"0";
+            break;
+    
+        case "Marron":
+            Resultado=Resultado+"1";
+            break;
+    
+        case "Rojo":
+            Resultado=Resultado+"2";
+            break;
+        
+        case "Naranja":
+            Resultado=Resultado+"3";
+            break;
+    
+        case "Amarillo":
+            Resultado=Resultado+"4";
+            break;
+    
+        case "Verde":
+            Resultado=Resultado+"5";
+            break;
+    
+        case "Azul":
+            Resultado=Resultado+"6";
+            break;
+    
+        case "Violeta":
+            Resultado=Resultado+"7";
+            break;
+    
+        case "Gris":
+            Resultado=Resultado+"8";
+            break;
+    
+        case "Blanco":
+            Resultado=Resultado+"9";
+            break;
+        
+        default:
+            //Aqui que le mandara al usuario un aviso de que falta banda 1
+        break; 
+    
+    }
+
+//Banda Multiplicadora
+
+
+
+    switch(multiplicadora)
+    {
+        case "Negro":
+            Resultado=Resultado;
+        break;
+
+        case "Marron":
+            Resultado=Resultado*10;
+            break;
+    
+        case "Rojo":
+            Resultado=Resultado*100;
+            break;
+        
+        case "Naranja":
+            Resultado=Resultado*1000;
+            break;
+    
+        case "Amarillo":
+            Resultado=Resultado*10000;
+            break;
+    
+        case "Verde":
+            Resultado=Resultado*100000;
+            break;
+    
+        case "Azul":
+            Resultado=Resultado*1000000;
+            break;
+    
+        case "Violeta":
+            Resultado=Resultado*10000000;
+            break;
+    
+        case "Gris":
+            Resultado=Resultado*100000000;
+            break;
+    
+        case "Blanco":
+            Resultado=Resultado*1000000000;
+            break;
+        
+        default:
+            //Aqui que le mandara al usuario un aviso de que falta banda 1
+        break; 
+
+    }
+    
+    Resultado=parseInt(Resultado); //Comvierto en un entero mi Resultado
+
+    document.getElementById('mi_resultado').innerHTML=Resultado;
+    
+    return Resultado;
+    
+    
+}
